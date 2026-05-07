efc2d04f2d2fb0e4740b4d9ffae10aa45f315a86
Evan <evansmalley2@gmail.com>
2026-05-07 19:54:02 -0400

    nextjs things

--- app/template.tsx
+++ app/template.tsx
@@ -8,7 +8,7 @@ import Header from 'components/Header';
 import { Style } from '@esmalley/ts-utils';
 
 import Sidebar from 'components/Sidebar';
-import { Dimensions, ThemeProvider, Themes, useWindowDimensions, UXBaseline } from '@esmalley/react-material-ui';
+import { ThemeProvider, Themes, useWindowDimensions, UXBaseline } from '@esmalley/react-material-ui';
 
 
 const localStorageKey = 'theme';
@@ -29,9 +29,15 @@ export const set_theme_mode = (mode: Themes) => {
 
 
 const Template = ({ children }: { children: React.ReactNode }) => {
-  const { width } = useWindowDimensions() as Dimensions;
+  const { width } = useWindowDimensions();
 
   const [themeMode, setTheme] = useState<Themes>(getInitialMode);
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   // Register/unregister the setter on mount/unmount
   useEffect(() => {
@@ -49,17 +55,23 @@ const Template = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider theme={themeMode}>
       <UXBaseline />
-      <Header />
-      <div
-        className={Style.getStyleClassName({ paddingTop, height: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden', boxSizing: 'border-box' })}
-      >
-        <div className={Style.getStyleClassName({ display: 'flex', flex: 1, overflow: 'hidden' })}>
-          <Sidebar />
-          <div className={Style.getStyleClassName({ width: '100%', flexShrink: 1, overflowY: 'auto', padding: '10px 10px 56px 10px' })}>
-            {children}
+      {
+        isMounted ?
+        <>
+          <Header />
+          <div
+            className={Style.getStyleClassName({ paddingTop, height: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden', boxSizing: 'border-box' })}
+          >
+            <div className={Style.getStyleClassName({ display: 'flex', flex: 1, overflow: 'hidden' })}>
+              <Sidebar />
+              <div className={Style.getStyleClassName({ width: '100%', flexShrink: 1, overflowY: 'auto', padding: '10px 10px 56px 10px' })}>
+                {children}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+        : ''
+      }
     </ThemeProvider>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+'use client';
 
 import { usePathname } from 'next/navigation';
 import { Objector, Style } from '@esmalley/ts-utils';
